e84b87ae5d616370346b36a519093d44ab3acce5
ravi23082000 <ravi23082000@gmail.com>
2017-11-09 15:10:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+var canvas = document.getElementById("myCanvas");
+var finalcolor = document.getElementsByClassName("finalcolor")[0];
+var showingcolor = document.getElementsByClassName("showingcolor")[0];
+var rangecolor = document.getElementsByClassName("rangecolor");
+var checkcolor = document.getElementsByClassName("checkcolor")[0];
+var initcolor = document.getElementsByClassName("initcolor");
+var setcolor = document.getElementsByClassName("setcolor")[0];
+var initialcolor = document.getElementsByClassName("initialcolor")[0];
+
+var x=y=z = 0;
+var color = "#fc4c4f"
+
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+
+checkcolor.style.backgroundColor = rgb(0,0,0);
+finalcolor.style.display = "none"
+
+
+showingcolor.onclick = visibilityof()
+
+setcolor.onclick = function(){
+	var morecolor = document.createElement("span");
+	morecolor.classList.add("initcolor");
+	morecolor.style.backgroundColor = rgb(x,y,z)
+	initialcolor.appendChild(morecolor);
+
+
+	for (var i = 0; i < initcolor.length; i++) {
+	    initcolor[i].addEventListener("click", changingclass);
+	}
+}
+
+showingcolor.onclick = function(){
+	if(finalcolor.style.display == "none"){
+		finalcolor.style.display = "block"
+	}else{
+		finalcolor.style.display = "none"
+	}
+}
+
+
+function visibilityof(){
+	if (finalcolor.style.visibility === "visible"){
+	finalcolor.style.visibility = "none"
+}
+else{
+	finalcolor.style.visibility = "visible"
+}
+}
+
+function changingclass(){
+	if (this.classList.contains("selected") == false) {
+		document.getElementsByClassName("selected")[0].classList.remove("selected");
+		this.classList.add("selected");
+		color = document.getElementsByClassName("selected")[0].style.backgroundColor
+	}
+	else{//esut
+	}
+}
+
+for (var i = 0; i < initcolor.length; i++) {
+    initcolor[i].addEventListener("click", changingclass);
+}
+
+function rgb(r, g, b){
+  return "rgb("+r+","+g+","+b+")";
+}
+
+
+rangecolor[0].onmouseup = function(){
+	x = Number(this.value)
+	checkcolor.style.backgroundColor = rgb(x,y,z);
+}
+rangecolor[1].onmouseup = function(){
+	y = Number(this.value)
+	checkcolor.style.backgroundColor = rgb(x,y,z);
+}
+rangecolor[2].onmouseup = function(){
+	z = Number(this.value)
+	checkcolor.style.backgroundColor = rgb(x,y,z);
+}
+
+
+
+
+var ctx = canvas.getContext("2d")
+
+
+canvas.onmousedown = function(e){
+	lastEvent = e;
+  	mouseDown = true;
+}
+
+canvas.onmousemove = function(e){
+	if(mouseDown){
+		var rect = canvas.getBoundingClientRect()
+		ctx.beginPath();
+	    ctx.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+	    ctx.lineTo(e.offsetX, e.offsetY);
+	    ctx.strokeStyle = color;
+	    ctx.stroke();
+	    lastEvent = e;
+	}
+}
+
+canvas.onmouseup = function(){
+	mouseDown = false;
+}
+
+canvas.onmouseleave = function(){
+	canvas.onmouseup();
+}
